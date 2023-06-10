c961339191dd1494d77559bfcc9562b305580a96
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2023-06-10 16:19:39 +0300

    Initial Commit

---
+++ AlbumApp_HW14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumApp_HW14
+//
+//  Created by D K on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
